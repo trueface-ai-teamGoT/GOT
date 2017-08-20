5f5a46f9c7978e014b73a3d7fb03d1e4b227e82a
ladybugtju <ines.bahri@yahoo.com>
2017-08-20 14:56:26 -0700

    Adding templates

--- GOTRecognition.py
+++ GOTRecognition.py
@@ -1,10 +1,12 @@
-from flask import Flask,requests, redirect, url_for, render_template
+
+from flask import Flask, redirect, url_for, render_template
 import os
 
 
 app = Flask(__name__)
 
 from Backend import trueface
+import requests
 import time
 import base64
 
@@ -12,21 +14,18 @@ import base64
 
 @app.route('/', methods=['POST'])
 def index():
- 
+     
+    # return  base64.b64encode(open('Testimages/jon.jpg','rb').read()).decode('utf-8')
+    # name, conf =  base64.b64encode(open('Testimages/jon.jpg','rb').read()).decode('utf-8')
+
+
     if request.method == "POST":
- 
-        image = request
-
-        print request
- 
+        image = request.form.get('img')
         try:
-
-			name, conf = trueface.apiIdentify(image).read()).decode('utf-8'))	
-
-	    return name, conf
-	 
+			name, conf = trueface.apiIdentify(base64.b64encode(image.read()).decode('utf-8'))	
+	        return render_template("index.html", data=(name, conf))
         except:
-            jsonify({"sorry": "Sorry, no results! Please try again."}), 500
+            jsonify({"sorry": "Sorry, no results! Please try again."}, 500)
 
 
 if __name__ == '__main__':
