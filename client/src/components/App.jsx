04a834540f064d284f5c11f152bb720cfc343276
stevenjchang <stevenjchang@gmail.com>
2017-08-20 12:55:47 -0700

    init App.jsx

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ImageUpload from './ImageUpload.jsx'
+
+class App extends React.Component {
+
+  render () {
+    return (
+      <ImageUpload></ImageUpload>
+    )
+  }
+}
+
+export default App
